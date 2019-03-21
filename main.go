0482513da5fae81f25ef60f60479bef55c9013b8
JJC <snake123722@gmail.com>
2019-03-21 12:28:13 +0800

    test .env by REPEAT print messages

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/russross/blackfriday"
 )
 
+var (
+	repeat int
+)
+
+func repeatHandler(c *gin.Context) {
+	var buffer bytes.Buffer
+	for i := 0; i < repeat; i++ {
+		buffer.WriteString("Hello from Go!\n")
+	}
+	c.String(http.StatusOK, buffer.String())
+}
+
 func main() {
+	var err error
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
+	}
+
+	tStr := os.Getenv("REPEAT")
+	repeat, err = strconv.Atoi(tStr)
+	if err != nil {
+		log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
+		repeat = 5
 	}
 
 	router := gin.New()
@@ -33,6 +55,8 @@ func main() {
 	router.GET("/test", func(c *gin.Context) {
 		c.String(http.StatusOK, string("Test 12345"))
 	})
+
+	router.GET("/repeat", repeatHandler)
 
 	router.Run(":" + port)
 }
